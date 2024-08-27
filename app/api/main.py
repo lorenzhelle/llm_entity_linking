feb229e2a243254d0f174908318c61d17106fd67
Lorenz Helle <lorenzhelle@web.de>
2024-08-27 22:09:20 +0200

    add google and fast api

---
+++ app/api/main.py
@@ -0,0 +1,53 @@
+import os
+import sys
+
+
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
+from typing import Any, Dict, List, Union
+from app.entity_linking import EntityLinking
+from app.foundation_models.chat_openai import AIModelType
+from app.schema import json_schema
+
+app = FastAPI()
+
+
+class FilterRequest(BaseModel):
+    message: str
+    schema: Dict[str, Any]
+    model: AIModelType
+
+
+class FilterResponse(BaseModel):
+    filter_generator_output: List[Dict[str, Any]]
+    recognized_filters: List[Dict[str, Any]]
+
+
+@app.post("/recognize-filters", response_model=FilterResponse)
+async def recognize_filters(request: FilterRequest):
+    llm_module = EntityLinking(schema=request.schema, model=request.model)
+
+    if (
+        request.model == AIModelType.MISTRAL_LARGE
+        or request.model == AIModelType.MISTRAL_MIXTRAL_8x22B
+        or request.model == AIModelType.MISTRAL_SMALL
+    ):
+        filter_generator_output = llm_module.generate_sync(conversation=request.message)
+    else:
+        filter_generator_output = await llm_module.generate_async(
+            conversation=request.message
+        )
+
+    recognized_filters = []
+
+    for filter in filter_generator_output:
+        filtered_dict = {
+            k: v for k, v in filter.model_dump().items() if v is not None and v
+        }
+
+        recognized_filters.append(filtered_dict)
+
+    return FilterResponse(
+        filter_generator_output=filter_generator_output,
+        recognized_filters=recognized_filters,
+    )
